7876f91db6ac00515dfd5f3bc6bfbfac1ae869c9
Vladas Zakrevskis <146100@gmail.com>
2024-07-27 01:08:37 +0300

    removed unused comment

--- tain/src/postgres.rs
+++ tain/src/postgres.rs
@@ -35,31 +35,3 @@ impl Postgres {
         Ok(())
     }
 }
-
-// #!/usr/bin/env python3
-//
-// import os
-//
-//
-// def run(string):
-// print(string)
-// if os.system(string):
-// raise Exception("Shell script has failed")
-//
-//
-// pg_data = "/spesogon_pg"
-//
-// source = os.path.normpath(os.path.expanduser("~/spesogon_pg"))
-//
-// print(source)
-//
-// run(f"sudo docker run \
-//     --name postgres_spesogon_test \
-//     --mount type=bind,source={source},target={pg_data} \
-//     --cap-add=SYS_PTRACE \
-//     --security-opt seccomp=unconfined \
-//     -p 5432:5432 \
-//     -e POSTGRES_PASSWORD=1111 \
-//     -e POSTGRES_DB=spesogon \
-//     -e PGDATA={pg_data} \
-//     -d postgres:16.2-alpine")
