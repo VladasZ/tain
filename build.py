44946067cb57d6be8c65140cc635923ec2d00b8d
Vladas Zakrevskis <146100@gmail.com>
2025-04-07 02:18:04 +0300

    build with cross

--- build.py
+++ build.py
@@ -3,35 +3,20 @@
 import os
 import shutil
 import subprocess
-from pathlib import Path
-
-TARGET = "aarch64-unknown-linux-gnu"
-DOCKER_IMAGE = "rust:latest"
 
 
-def build_with_docker():
-    print("🔧 Starting Docker build...")
+def build():
+    print("🔧 Starting build...")
 
-    docker_cmd = (
-        f'docker run --rm '
-        f'-v "{Path.cwd()}:/usr/src/myapp" '
-        f'-w /usr/src/myapp '
-        f'{DOCKER_IMAGE} '
-        f'bash -c "dpkg --add-architecture arm64 && apt update && '
-        f'apt install -y gcc-aarch64-linux-gnu libc6-dev-arm64-cross libssl-dev:arm64 pkg-config && '
-        f'rustup target add {TARGET} && '
-        f'export PKG_CONFIG_ALLOW_CROSS=1 && '
-        f'export OPENSSL_DIR=/usr/aarch64-linux-gnu && '
-        f'export OPENSSL_LIB_DIR=/usr/lib/aarch64-linux-gnu && '
-        f'export OPENSSL_INCLUDE_DIR=/usr/include/openssl && '
-        f'cargo build --all --release --target={TARGET}"'
-    )
+    result = subprocess.run("cargo install cross --git https://github.com/cross-rs/cross", shell=True)
+    if result.returncode != 0:
+        raise RuntimeError("❌ Install cross failed.")
 
-    result = subprocess.run(docker_cmd, shell=True)
+    result = subprocess.run("cross build --release --all --target aarch64-unknown-linux-gnu", shell=True)
     if result.returncode != 0:
         raise RuntimeError("❌ Build failed.")
 
-    print(f"✅  Done! Check ./target/{TARGET}/release/")
+    print(f"✅ Build done!")
 
 
 def gather_artifacts():
@@ -39,10 +24,9 @@ def gather_artifacts():
     dst = 'artifacts/tain'
 
     os.makedirs(os.path.dirname(dst), exist_ok=True)
-
     shutil.copy2(src, dst)
 
 
 if __name__ == "__main__":
-    build_with_docker()
+    build()
     gather_artifacts()
